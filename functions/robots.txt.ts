c1c31d6d5842da0826ca6028082d2197e2f8b95d
Cedar <cedaren@gmail.com>
2026-04-28 17:00:11 +0800

    feat: update robots.txt to include sitemap locations and allow indexing

--- functions/robots.txt.ts
+++ functions/robots.txt.ts
@@ -3,7 +3,14 @@ export const onRequestGet = async (context: any) => {
     const baseUrl = `${url.protocol}//${url.host}`;
 
     const robots = `User-agent: *
-Disallow:`;
+Allow: /
+Sitemap: ${baseUrl}/sitemap.xml
+Sitemap: ${baseUrl}/sitemaps/sitemap-prompts.xml
+Sitemap: ${baseUrl}/sitemaps/sitemap-categories.xml
+Sitemap: ${baseUrl}/sitemaps/sitemap-topics.xml
+Sitemap: ${baseUrl}/sitemaps/sitemap-authors.xml
+Sitemap: ${baseUrl}/sitemaps/sitemap-tags.xml
+Sitemap: ${baseUrl}/sitemaps/sitemap-pages.xml`;
 
     return new Response(robots, {
         headers: { "Content-Type": "text/plain" },
